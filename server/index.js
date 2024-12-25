1236edfd8f4e47995db1ec6aa5f1b636effa227c
SlayerDevv <hdfhoussam18@gmail.com>
2024-12-25 03:36:30 +0100

    added new files

--- server/index.js
+++ server/index.js
@@ -1,20 +1,32 @@
-const express = require('express');
-const app = express()
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const helmet = require('helmet');
-const port = 3000
-app.use(cors())
+const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const { Connect } = require("./lib/db");
+require("dotenv").config();
+const port = 5000;
+//
+const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
+//
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-    }
-)
+//
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+const server = app.listen(port, async () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+  await Connect(process.env.DATABASE_URL);
+});
+
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
